1c4f5c1fa5e4dde349d6b599c40c353193ef5cca
Sujal Patel <110847211+patelsujal29@users.noreply.github.com>
2024-05-29 10:58:03 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,111 @@
-<html>
-<head>
-	<title> E-Home's Services </title>
-	<style>
-		.heyTextClass{
-			text-decoration-style: Bold ;
-			font-size: 30px ;
-			margin-top: 110px ;
-		}
-	.pos{
-		background-image: url(Professional1.jfif);
-		height: 100%;
-		width: 100%;
-		background-size: cover;
-		background-position: center;
-		background-repeat: no-repeat;
-		background-attachment: fixed;
+<?php
+session_start();
 
-	}
-	.btn {
-			box-shadow: none;
-			width: 15%;
-			height: 40px;
-			background-color: #138808;
-			color: #fff;
-			border-radius: 25px;
-			letter-spacing: 1.3px
-	}
-	.btn:hover {
-      background-color: #8cc751
+if(isset($_POST['Loginbtn']))
+{
+
+
+
+$con = mysqli_connect('localhost','root','','testproject');
+$username = $_POST["Emailid"];
+$password = $_POST["password"];
+$sql = "SELECT * FROM `worker` WHERE EMailId ='$username'  and WorkerPassword = '$password'";
+$query=mysqli_query($con,$sql);
+
+
+
+$num_rows = mysqli_num_rows($query);
+if($num_rows == 1) {
+	$raw=mysqli_fetch_array($query);
+$_SESSION["wid"] = $raw[0];
+  header('Location: Workerdashbord.php');
+
+}
+ else
+ {
+	echo "you are not a valid user!";
+ }
+ }
+/*if($query == true) {
+  echo '<a href="ClientLogin.php">Error: cannot execute query</a>';
+  exit;
+}*/
+
+
+/*$num_rows = mysql_num_rows($query);
+if($num_rows == 1) {
+  $_SESSION["login"] = "OK";
+  $_SESSION["username"] = $username;
+ $redirect = "Homepage.php";
+}
+else
+ $redirect = "ClientLogin.php";
+
+mysql_free_result($query);
+mysql_close($link);
+
+header("Location: $redirect");*/
+
+?>
+
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title> Login form for client </title>
+	<link rel = "stylesheet" href = "logine.css">
+</head>
+<body>
+		<style type="text/css">
+	.button{
+   height: 45px;
+   margin: 35px;
+ }
+.button input{
+   height: 100%;
+   width: 100%;
+   border-radius: 5px;
+   border: none;
+   color: #fff;
+   font-size: 18px;
+   font-weight: 500;
+   letter-spacing: 1px;
+   cursor: pointer;
+   transition: all 0.3s ease;
+   background: linear-gradient(-135deg, #8cc751, #138808);
+ }
+.button input:hover{
+  /* transform: scale(0.99); */
+    background: linear-gradient(-135deg, #138808, #8cc751);
   }
 	</style>
-	<link rel = "stylesheet">
-</head>
-<body class="pos">
+<table>
+    <div class="container">
+	<div class="content">
+	<div class="logo"> <img src="IMAGES\logo4-removebg-preview(1).png" alt="logo"> </div><br>
+	<div  class="title"> E-Home's Services </div>
+      <form action="index.php" method="post"><br>
+	<div class="user-details">
+        <div class="input-box">
+	  <label>Email Address</label>
+          <input type="text" name="Emailid" id="ClientId" required>
+        </div><br>
+        <div class="input-box">
+        <label>Password</label>  
+				<input type="password" name="password" id="Clientpwd" required>
+        </div>
+	<div class="input-box">
+	<br> <a href="WorkerSignup.php">New user??</a>
+	
+	  <div class="button input" >
+          <input type="submit" name="Loginbtn" value="Login">
+        </div>  
+	</div>
 
-			<div class="logo"> <img src="IMAGES\logo4-removebg-preview(1).png" alt="logo"> </div><br>
-			<center>
-			<b><div class="heyTextClass"> Hey...!! sss </div></b><br>
-			<b><div class="text-center mt-4 name" style="font-size: 30px;"> Welcome to our website E-Home's Services </div><br></b>
-
-			<a href="ClientLogin.php">
-						<button class="btn mt-3">Let's get started</button>
-			</a>
-		</center>
-
-</body>
+	
+	
+      </form>
+	</div>
+    </div>
+	</table>
+  </body>
 </html>
